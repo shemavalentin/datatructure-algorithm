666d6bf99d212da6a05960775563e6b0d5b90b2e
Shema Valentin <shemavalentin@gmail.com>
2024-04-21 09:00:12 +0200

    Adding loops for every element

---
+++ BigO/O(n!).js
@@ -0,0 +1,4 @@
+
+O(n!);  // It means you are adding for loop for every element.
+// This is very rare to be used and it a bad idea to use the function that has this Big O
+        // It is very expensive
