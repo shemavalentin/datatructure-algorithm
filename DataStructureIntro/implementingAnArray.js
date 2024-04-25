5f04e6a88d24c9228b2636036ea5ce238f0be0ae
Shema Valentin <shemavalentin@gmail.com>
2024-04-25 17:12:28 +0200

    Implemented array data structure

---
+++ DataStructureIntro/implementingAnArray.js
@@ -0,0 +1,62 @@
+// let's create an array using Classes
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  // Getting access/grab the data from memory.
+
+  get(index) {
+    return this.data[index];
+  }
+
+  // Let's push an Item into the array
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  // Let's delete the last Item
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    // Function that shift data to their new indexes after deletion
+    this.shiftItems(index);
+
+    return item;
+  }
+  // Method shiftItems to loop though items int the array
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+// Now copy this array
+
+const newArray = new MyArray();
+newArray.push("hi");
+newArray.push("Lola");
+newArray.push("Valentin");
+//newArray.pop();
+
+newArray.delete(1);
+
+newArray.push("!");
+
+//console.log(newArray.get(0));
+console.log(newArray);
---
+++ DataStructureIntro/staticVsDynamicArray.js
@@ -0,0 +1,10 @@
+// Difference between Static Arrays and Dynamic Arrays
+
+// => Static arrays are limitted in size: You need to specify number of elements your array will hold ahead of time
+// => Dynamic arrays: we can add on them other elements.
+
+// in C++ which uses static arrays ( It is a low level language) and it is why it is faster that high level
+// It manages memory.
+
+int a[20];
+int b[5] { 1,2,3,4,5} // a static array
