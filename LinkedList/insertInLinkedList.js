e1e513d30532780807233c05ec3306f2a357d6be
Shema Valentin <shemavalentin@gmail.com>
2024-05-06 17:38:21 +0200

    Insert in a linked list

--- LinkedList/insertInLinkedList.js
+++ LinkedList/insertInLinkedList.js
@@ -19,7 +19,7 @@ class LinkedList2 {
 
     this.tail.next = newNode2;
     this.tail = newNode2;
-    //this.length++;
+    this.length++;
     return this;
   }
 
@@ -27,7 +27,6 @@ class LinkedList2 {
 
   prepend(value) {
     // Here we can check inputs
-
     const addToHead = {
       value: value,
       next: null,
@@ -40,12 +39,11 @@ class LinkedList2 {
   }
 
   // Creating printList() to print out the array format output and easy to read as the list grows
-
   printList() {
     const array = [];
     let currentNode = this.head;
     while (currentNode !== null) {
-      array.push(currentNode);
+      array.push(currentNode.value);
       // Keep going untill the next node is null
       currentNode = currentNode.next;
     }
@@ -56,17 +54,58 @@ class LinkedList2 {
   // INSERT IN A LINKED LIST
 
   insert(index, value) {
+    if (index === 0) {
+      this.append(value);
+      return this.printList();
+    }
+
     // Cheking params
-    if (index >= this.length) {
+    else if (index >= this.length) {
       return this.append(value);
     }
+
+    // Now the hard part to insert on index 2 or 3 depending on the length of the array
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+
+    // But we have cut off the connection(pionter) btn nodes at i[2] and [3]. we need to keep it.
+    // next of the leader node is the new node
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    // Linking the holdingPointer and newNode
+    newNode.next = holdingPointer;
+    // We need to increament in the array cz indexing will change.
+    this.length++;
+    return this.printList();
   }
+
+  // Making our traversal(for loop)
+
+  traverseToIndex(index) {
+    // Check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  // REMOVING A VALUE
+
+  remove(index) {}
 }
+//    *->*    The first is the leader index and we need to insert btn the leader and the next
+//     *
+//
 
 const myLinkedListEx = new LinkedList2(15);
 myLinkedListEx.append(5);
 myLinkedListEx.append(10);
 myLinkedListEx.append(25);
 myLinkedListEx.prepend(7);
-myLinkedListEx.insert(200, 15);
-myLinkedListEx.printList();
+myLinkedListEx.insert(0, 99);
