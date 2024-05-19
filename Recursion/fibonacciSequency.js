2008247812943f7fe6fe394d504759926b30a025
Shema Valentin <shemavalentin@gmail.com>
2024-05-19 16:44:53 +0200

    Fibonacci Iterative

--- Recursion/fibonacciSequency.js
+++ Recursion/fibonacciSequency.js
@@ -5,7 +5,15 @@
 // The pattern of the sequency is that each value is the sum of the 2 previous values.
 // That means that for N = 5 -> 2+3
 
-function fibonacciIterative(n) {}
+function fibonacciIterative(n) {
+  // O(n+2) Cze we skipped 2 element but by removing indixes it is O(n)
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+
+  return arr[n];
+}
 
 fibonacciIterative(3);
 
@@ -13,7 +21,8 @@ function fibonacciRecursive(n) {
   if (n < 2) {
     return n;
   }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2); // O(2^n). The time complexity increases as we raise up inputs and it takes longer and longer.
+  //It is readable but it is not an ideal solution.
 }
 
-fibonacciRecursive(3);
+fibonacciRecursive(43);
