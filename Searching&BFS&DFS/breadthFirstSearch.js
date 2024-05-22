8983c0f710e465881d2698c40078aedfb02b2d3b
Shema Valentin <shemavalentin@gmail.com>
2024-05-22 13:00:07 +0200

    BFS & DFS

---
+++ Searching&BFS&DFS/breadthFirstSearch.js
@@ -0,0 +1,12 @@
+// this uses TRAVERSAL METHOD(BFS)
+
+// In this type of search, it traverses every node from left to right untill reaches the target.
+
+// This has memory implication, it consumes more memoy as we need to traverse each nodes.
+
+// Think of BSF as water flowing. It goes level by level
+
+// their time complexity is O(n)
+
+// PROS :  - shortes path, Closer Nodes
+// CONS: More memory
---
+++ Searching&BFS&DFS/depthFirstSearch.js
@@ -0,0 +1,9 @@
+// Unlike BFS, the DFS traverses each node from left but waits to finish checking with the left
+// nodes, veryfies each and every node on the left handside with right at each node
+// then go to the right hand side but from left-right.
+
+//  Time complexity o(n) , When traversing over the tree we don't repeat ourselves
+
+// PROS: Less memory, does path exist?: it also looks if the path exist
+
+// CONS: Can get Slow
